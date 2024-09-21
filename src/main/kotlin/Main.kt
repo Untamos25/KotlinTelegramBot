ba18b568cd0e8f52916f6f750953ced57cdff714
Stanislav <pavlushinsa18@gmail.com>
2024-09-21 19:57:34 +0300

    KTB-04 Созданы сущность Word и список dictionary. Реализовано добавление объектов типа Word в dictionary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,25 @@
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0
+)
+
 fun main() {
 
-    val wordsFile : File = File("words.txt")
+    val wordsFile: File = File("words.txt")
+    val lines: List<String> = wordsFile.readLines()
+    val dictionary: MutableList<Word> = mutableListOf()
 
-    wordsFile.readLines().forEach {
+    for (line in lines) {
+        val line = line.split("|")
+        val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
+        dictionary.add(word)
+    }
+
+    dictionary.forEach {
         println(it)
     }
+
 }
